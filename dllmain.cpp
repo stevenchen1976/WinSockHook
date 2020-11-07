6136eb64a1df83e883f723cc8deb551377e11582
iSynex <zerobytepm@gmail.com>
2020-11-07 02:44:28 +0200

    Removed Ex methods

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,4 +1,5 @@
-﻿#include <cstdio>
+﻿#include "pch.h"
+#include <cstdio>
 #include <ctime>
 #include <fstream>
 #include <iomanip>
@@ -6,26 +7,19 @@
 #include <windows.h>
 #include "detours.h"
 #include <iostream>
+#include <winsock.h>
 
 #pragma comment(lib,"Ws2_32.lib") 
 #pragma comment( lib, "detours.lib" )
-#pragma comment( lib, "detoured.lib" )
-#pragma comment( lib, "Mswsock.lib" )
-#pragma comment(lib, "detours.lib")
-
 
 using namespace std;
 
 
 int (WINAPI* Real_Send)(SOCKET s, const char* buf, int len, int flags) = send;
 int (WINAPI* Real_Recv)(SOCKET s, char* buf, int len, int flags) = recv;
-int (WINAPI* Real_RecvFrom)(SOCKET s, char* buf, int len, int flags, sockaddr* from, int* fromlen) = recvfrom;
-int (WINAPI* Real_WSARecvEx)(SOCKET s, char* buf, int len, int* flags) = WSARecvEx;
 
 int WINAPI Mine_Send(SOCKET s, const char* buf, int len, int flags);
 int WINAPI Mine_Recv(SOCKET s, char* buf, int len, int flags);
-int WINAPI Mine_RecvFrom(SOCKET s, char* buf, int len, int flags, sockaddr* from, int* fromlen);
-int WINAPI Mine_WSARecvEx(SOCKET s, char* buf, int len, int* flags);
 
 int WINAPI Mine_Send(SOCKET s, const char* buf, int len, int flags) {
     
@@ -39,17 +33,6 @@ int WINAPI Mine_Recv(SOCKET s, char* buf, int len, int flags) {
     return Real_Recv(s, buf, len, flags);
 }
 
-int WINAPI Mine_RecvFrom(SOCKET s, char* buf, int len, int flags, sockaddr* from, int* fromlen) {
-    
-    cout << "Packet RecvFrom: " << buf << endl;
-    return Real_RecvFrom(s, buf, len, flags, from, fromlen);
-}
-
-int WINAPI Mine_WSARecvEx(SOCKET s, char* buf, int len, int* flags) {
-    
-    cout << "Packet RecvEx: " << buf << endl;
-    return Real_WSARecvEx(s, buf, len, flags);
-}
 
 BOOL WINAPI DllMain(HINSTANCE, DWORD dwReason, LPVOID) {
     switch (dwReason) {
@@ -62,8 +45,6 @@ BOOL WINAPI DllMain(HINSTANCE, DWORD dwReason, LPVOID) {
         DetourUpdateThread(GetCurrentThread());
         DetourAttach(&(PVOID&)Real_Send, Mine_Send);
         DetourAttach(&(PVOID&)Real_Recv, Mine_Recv);
-        DetourAttach(&(PVOID&)Real_RecvFrom, Mine_RecvFrom);
-        DetourAttach(&(PVOID&)Real_WSARecvEx, Mine_WSARecvEx);
         DetourTransactionCommit();
 
         break;
@@ -74,8 +55,6 @@ BOOL WINAPI DllMain(HINSTANCE, DWORD dwReason, LPVOID) {
         DetourUpdateThread(GetCurrentThread());
         DetourDetach(&(PVOID&)Real_Send, Mine_Send);
         DetourDetach(&(PVOID&)Real_Recv, Mine_Recv);
-        DetourDetach(&(PVOID&)Real_RecvFrom, Mine_RecvFrom);
-        DetourDetach(&(PVOID&)Real_WSARecvEx, Mine_WSARecvEx);
         DetourTransactionCommit();
     }
 }
